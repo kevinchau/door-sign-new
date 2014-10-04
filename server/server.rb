2e819876ef4f4728b1ebda3eb699deab3368bee0
Kevin Chau <kevin@kevinchau.org>
2014-10-04 00:08:39 -0700

    Mostly Done minus a few TODOs

--- server/server.rb
+++ server/server.rb
@@ -8,36 +8,184 @@ require 'active_support/core_ext'
 require 'open-uri'
 require 'Date'
 
+#Info
+NAME = "Kevin"
+TZONE = "America/Los_Angeles"
+SPARK_ID = ENV['SPARK_ID']
+SPARK_TOKEN = ENV['SPARK_TOKEN']
+
+#create new array
+timeEvents = []
+alldayEvents = []
+
+#ICS URLs
+URLS = ENV['ICS_URL_LIST']
+
+### Parse all calendar events ###
 
 #Open a calendar
 calendars = nil
-open("CALENDAR HERE") do |cal|
 
-  #Parses ics file into calendars
-  calendars = Icalendar.parse(cal)
-end
+URLS.each do |url|
 
-#TODO: handle all day events.
+  open(url) do |cal|
 
-#create new array
-events = []
+    #Parses ics file into calendars
+    calendars = Icalendar.parse(cal)
+  end
 
-calendars.each do |calendar|
 
-  calendar.events.each do |event|
+  calendars.each do |calendar|
 
-    #Parses Datetime from .ics
-    startTime = DateTime.strptime(event.dtstart.to_s, "%Y-%m-%d %H:%M:%S %z")
-    endTime = DateTime.strptime(event.dtend.to_s, "%Y-%m-%d %H:%M:%S %z")
+    calendar.events.first(15).each do |event|
 
-    #Only selects event if current time is between event start and event end
-    if Time.now.to_i < endTime.to_i and Time.now.to_i > startTime.to_i
+      #set vars for start and end time
+      start_time = event.dtstart.value
+      end_time = event.dtend.value
 
-      #put event into array
-      events << {event: event.summary, location: event.location}
-      puts events
+
+      #Determine if event is timed or all-day
+      if event.dtstart.value.is_a?(Date)
+
+        #do stuff for all day event
+        if Date.today >= start_time && Date.today <= end_time
+
+          #add event to array
+          alldayEvents << {event:event.summary, location: event.location}
+
+        end
+
+      elsif event.dtstart.value.is_a?(ActiveSupport::TimeWithZone)
+
+        #do stuff for time event
+        if Time.now >= start_time && Time.now <= end_time
+
+          #add event to array
+          timeEvents << {event: event.summary, location: event.location, eventEnd: event.dtend.in_time_zone('America/Los_Angeles').strftime("%I:%M %p")}
+
+        end
+
+      end
+
     end
 
   end
+
+end
+
+### Events Logic ###
+
+#Timed event handling
+
+#TODO: Choose event that ends later
+
+#if there is more than 1 all-day event
+if timeEvents.length > 1
+
+  #select based on time
+  if Time.now.min.odd?
+    timeEvent = timeEvents[0]
+  else
+    timeEvent = timeEvents[1]
+  end
+
+elsif
+
+  #if just one, pick first
+  timeEvent = timeEvents.first
+
+else
+
+  timeEvent = nil
+
+end
+
+#All day event handling
+
+if alldayEvents.length > 1
+
+  #pick one based on time
+  if Time.now.min.odd?
+    dayEvent = alldayEvents[0]
+  else
+    dayEvent = alldayEvents[1]
+  end
+
+elsif alldayEvents.length == 1
+
+  dayEvent = alldayEvents.first
+
+else
+
+  dayEvent = nil
+
+end
+
+#Events Display
+
+#TODO: Build logic to determine free at
+
+#if no events
+if dayEvent.nil? && timeEvent.nil?
+  line0 = "#{NAME} is Currently:".truncate(20)
+  line1 = "  Not Busy".truncate(20)
+  line2 = "Come say hello!".truncate(20)
+  line3 = "Updated:#{Time.now.in_time_zone(TZONE).strftime("%I:%M %p")}".truncate(20)
+
+#if time event ONLY
+elsif timeEvent && dayEvent.nil?
+  line0 = "#{NAME} is at:".truncate(20)
+  line1 = "#{timeEvent[:event]}".truncate(20)
+  line2 = "#{timeEvent[:location]}".truncate(20)
+  line3 = "Ends at #{timeEvent[:eventEnd]}".truncate(20)
+
+#if both time event and all-day event
+elsif timeEvent && dayEvent
+  line0 = "#{NAME} is at:".truncate(20)
+  line1 = "#{timeEvent[:event]}".truncate(20)
+  line2 = "#{timeEvent[:location]}".truncate(20)
+  line3 = "(#{dayEvent[:event]}".truncate(19) + ")"
+
+#if all-day event ONLY
+elsif dayEvent && timeEvent.nil?
+  line0 = "#{NAME} has an".truncate(20)
+  line1 = "All-Day Event.".truncate(20)
+  line2 = "If Im here,Im Not".truncate(20)
+  line3 = "Busy.Come say hi! :)".truncate(20)
+
+end
+
+payload = "#{line0}|#{line1}|#{line2}|#{line3}"
+
+### Spark Core Stuff ###
+
+#Initiate Spark Core
+core = RubySpark::Core.new(SPARK_ID, SPARK_TOKEN)
+
+#Send text to core
+
+core.function("update", payload)
+
+# Figure out whether the backlight should be on or off
+now = Time.now.in_time_zone(TZONE)
+
+# Compare them
+if now.hour > 23 or now.hour < 7
+
+  # Turn it off at night
+  core.function("backlight", "off")
+
+elsif now.wday == 0
+
+  core.function("backlight", "off")
+
+elsif now.wday == 6
+
+  core.function("backlight", "off")
+
+else
+
+  # Turn it on during the day
+  core.function("backlight", "on")
 
 end
